f6ddeca5ceb713cd92ce9f2ab43023212a68d142
Jeffersontavaresdev@gmail.com <Jeffersontavaresdev@gmail.com>
2023-07-18 18:20:50 -0300

    Inicio no curso

---
+++ inicio/comentarios.js
@@ -0,0 +1,11 @@
+// Comentários de linha
+/*Comentários de blocos*/
+
+console.log("Ola, Mundo"); //print
+
+let preco = 100.00;
+let desconto = 5;
+
+let descontofinal = preco - (preco * 5 / 100);
+
+console.log(descontofinal);
